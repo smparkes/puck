68606bb5fa42aa3b7c943a3b4e83db39e79ed539
Steven Parkes <smparkes@smparkes.net>
2010-04-09 15:38:11 -0700

    wip

--- spec/lib/puck/state_spec.js
+++ spec/lib/puck/state_spec.js
@@ -48,10 +48,39 @@
         describe("publish_to", function(){
           beforeEach(function(){
             this.state = new State();
+            this.notify = function(){};
           });
           
           it("should exist and return the state object", function(){
             expect(this.state.publish_to(this)).toBe(this.state);
+          });
+
+          it("should publish the initial state if it exists", function(){
+            spyOn(this,"notify");
+            this.state.commit();
+            this.state.publish_to(this);
+            expect(this.notify).wasCalled();
+          });
+
+          it("should publish the state value", function(){
+            this.notify = function(state){
+              expect(state).toBe(this.state);
+              complete();
+            };
+            this.state.commit();
+            this.state.publish_to(this);
+            incomplete();
+          });
+
+          it("should not publish nonexistent initial state", function(){
+            spyOn(this,"notify");
+            this.state.publish_to(this);
+            expect(this.notify).wasNotCalled();
+          });
+
+          describe("events", function(){
+            beforeEach(function(){
+            });
           });
         });
       });
@@ -78,3 +107,42 @@
 
   });
 }(jQuery));
+
+/*
+
+{a: "b"} => {} => {}, {deleted: ["a"]}
+
+{a: ["b", "c"]} => {a: "b"}, {changed: ["a"]}
+
+{a: ["b", "c"]} => {a: ["b"]}, {deleted: {a: [1]}}
+
+{a: ["b", "c"]} => {a: ["b", "c", "d"]}, {added: {a: [{2: "d"}]}}
+
+{a: {b: [0]}} => {a: {b: [0, 1]}} => {added: {a: {b: {1: 1}}}}
+
+{a: {c: 0}} => {a: {b: 0, c: 1}} => {added: {a: {b: 0}}}
+ 
+{current: {id:..., value:}
+ previous: {id: ..., value: }
+ changed: {...}
+ changes: [...]}
+
+diff
+  toState
+    toJSON
+fromState
+applyDiff
+
+object.state(...)
+object.diff(...) =>
+{from: 1,
+ to: 2,
+ diff: [
+   {add: {a: b}}
+   {change: {a: b}}
+   {delete: {a: undefined}}
+ ]}
+object.apply_diff(...)
+
+
+*/
